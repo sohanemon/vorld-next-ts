6923c3e7ef12f3cd935ae284ddbda07156128b5a
sohanemon <m.sohanemon@gmail.com>
2023-06-24 09:22:09 +0600

    feat: navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,36 @@
+'use client';
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
-    <nav className='box'>
-      <Image src={'/images/logo.svg'} alt='logo' width={149} height={38} />
+    <nav className='flex items-center justify-between pt-3 shadow-2xl box-full shadow-theme-sky/10'>
+      <Image
+        src={'/images/logo.svg'}
+        alt='logo'
+        className='-mt-3'
+        width={149}
+        height={38}
+      />
+      <ul className='flex '>
+        {navs.map((_) => (
+          <Link
+            href={`/${_}`}
+            as={_ === 'home' ? '/' : _}
+            key={_}
+            className={cn('px-3 tracking-wide uppercase', {
+              'border-b-[6px] border-theme-sky':
+                pathname.includes(_) || (pathname === '/' && _ === 'home'),
+            })}
+          >
+            {_}
+          </Link>
+        ))}
+      </ul>
     </nav>
   );
 }
